70382f48e734d6cd18c20b0f90cddf19f2fa0982
SQclassroom <sq.vivek@gmail.com>
2023-12-11 14:04:40 +0530

    11-12-2023

--- 7.REACTJS/24.Topic-React_Routers_Hook/apolo/src/Pages/About.jsx
+++ 7.REACTJS/24.Topic-React_Routers_Hook/apolo/src/Pages/About.jsx
@@ -33,8 +33,6 @@ const About = () => {
       </div>
       <ul className="flex">
         <li><Link className="link" to="/">Go to Home Page</Link></li>
-        <li><Link className="link" to="/user/mark">Mark Page</Link></li>
-        <li><Link className="link" to="/user/peter">Peter Page</Link></li>
       </ul>
     </div>
   );
