10e9d0275b30c3a8016917b8b8714a4afb0d649b
vivek kachhadiya <ifmdac80@gmail.com>
2024-01-11 15:31:42 +0530

    11-01-2024

--- 7.REACTJS/35.Topic-React-Redux-ShoopingCart/apolo/src/pages/Shop.js
+++ 7.REACTJS/35.Topic-React-Redux-ShoopingCart/apolo/src/pages/Shop.js
@@ -8,108 +8,108 @@ const Shop = () => {
 
 
 
-  const { isCartOpen, cartItems } = useSelector((state) => state.cart);
+    const { isCartOpen, cartItems } = useSelector((state) => state.cart);
 
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
 
-  const handleCloseCart = (close) => {
-    dispatch(toggleCart(close));
-  };
+    const handleCloseCart = (close) => {
+        dispatch(toggleCart(close));
+    };
 
 
-  const handleRemove = (itemId) => {
-    dispatch(removeItem(itemId));
-  };
+    const handleRemove = (itemId) => {
+        dispatch(removeItem(itemId));
+    };
 
 
-  const handleIncrement = (itemId) => {
-    dispatch(incrementItem(itemId));
-  };
+    const handleIncrement = (itemId) => {
+        dispatch(incrementItem(itemId));
+    };
 
-  const handleDecrement = (itemId) => {
-    dispatch(decrementItem(itemId));
-  };
-
-
-  // disable the body-scroll when the Cart is open
-  // useEffect(() => {
-  //   const docBody = document.body;
-
-  //   isCartOpen ? (
-  //     docBody.classList.add('overflow_hide')
-  //   ) : (
-  //     docBody.classList.remove('overflow_hide')
-  //   );
-
-  // }, [isCartOpen]);
+    const handleDecrement = (itemId) => {
+        dispatch(decrementItem(itemId));
+    };
 
 
-  const cartQuantity = cartItems.length;
+    // disable the body-scroll when the Cart is open
+    // useEffect(() => {
+    //   const docBody = document.body;
 
-  const cartTotal = cartItems.map(item => item.price * item.quantity).reduce((prevValue, currValue) => prevValue + currValue, 0);
+    //   isCartOpen ? (
+    //     docBody.classList.add('overflow_hide')
+    //   ) : (
+    //     docBody.classList.remove('overflow_hide')
+    //   );
 
-  return (
-    <div>
-      <Header />
-      <h1>This is Shop Page</h1>
-                            <div className="cart_body">
-                                {
-                                    cartQuantity === 0 ? (
-                                        <h2>Cart is empty</h2>
-                                    ) : (
-                                        cartItems.map(item => {
-                                            const { id, img, title, price, quantity } = item;
-                                            const itemTotal = price * quantity;
+    // }, [isCartOpen]);
 
-                                            return (
-                                                <div className="cart_items" key={id}>
-                                                    <figure className="cart_items_img">
-                                                        <img src={img} alt="product-img" />
-                                                    </figure>
 
-                                                    <div className="cart_items_info">
-                                                        <h4>{title}</h4>
-                                                        <h3 className="price">₹ {itemTotal.toLocaleString()}</h3>
-                                                    </div>
+    const cartQuantity = cartItems.length;
 
-                                                    <div className="cart_items_quantity">
-                                                        <span onClick={() => handleDecrement(id)}>&#8722;</span>
-                                                        <b>{quantity}</b>
-                                                        <span onClick={() => handleIncrement(id)}>&#43;</span>
-                                                    </div>
+    const cartTotal = cartItems.map(item => item.price * item.quantity).reduce((prevValue, currValue) => prevValue + currValue, 0);
 
-                                                    <div
-                                                        title="Remove Item"
-                                                        className="cart_items_delete"
-                                                        onClick={() => handleRemove(id)}
-                                                    >
-                                                        <span>&times;</span>
-                                                    </div>
-                                                </div>
-                                            );
-                                        })
-                                    )
-                                }
-                            </div>
+    return (
+        <div>
+            <Header />
+            <h1>This is Shop Page</h1>
+            <div className="cart_body">
+                {
+                    cartQuantity === 0 ? (
+                        <h2>Cart is empty</h2>
+                    ) : (
+                        cartItems.map(item => {
+                            const { id, img, title, price, quantity } = item;
+                            const itemTotal = price * quantity;
 
-                            <div className="cart_foot">
-                                <h3>
-                                    <small>Total:</small>
-                                    <b>₹ {cartTotal.toLocaleString()}</b>
-                                </h3>
+                            return (
+                                <div className="cart_items" key={id}>
+                                    <figure className="cart_items_img">
+                                        <img src={img} alt="product-img" />
+                                    </figure>
 
-                                <button
-                                    type="button"
-                                    className="checkout_btn"
-                                    disabled={cartQuantity === 0}
-                                >
-                                    Checkout
-                                </button>
-    </div>
-    </div>
-  )
+                                    <div className="cart_items_info">
+                                        <h4>{title}</h4>
+                                        <h3 className="price">₹ {itemTotal.toLocaleString()}</h3>
+                                    </div>
+
+                                    <div className="cart_items_quantity">
+                                        <span onClick={() => handleDecrement(id)}>&#8722;</span>
+                                        <b>{quantity}</b>
+                                        <span onClick={() => handleIncrement(id)}>&#43;</span>
+                                    </div>
+
+                                    <div
+                                        title="Remove Item"
+                                        className="cart_items_delete"
+                                        onClick={() => handleRemove(id)}
+                                    >
+                                        <span>&times;</span>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    )
+                }
+            </div>
+
+            <div className="cart_foot">
+                <h3>
+                    <small>Total:</small>
+                    <b>₹ {cartTotal.toLocaleString()}</b>
+                </h3>
+
+                <button
+                    type="button"
+                    className="checkout_btn"
+                    disabled={cartQuantity === 0}
+                >
+                    Checkout
+                </button>
+            </div>
+        </div>
+    )
 }
 
-      export default Shop
+export default Shop
